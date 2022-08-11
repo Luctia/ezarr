b964a8b012528093501f9dd7026afb257ad0e137
Luctia <timmerman.luc1999@gmail.com>
2022-08-11 22:16:01 +0200

    Partial cleanup

--- users_groups_setup.py
+++ users_groups_setup.py
@@ -6,28 +6,55 @@ class UserGroupSetup:
         self.root_dir = root_dir
         os.system('sudo groupadd mediacenter -g 13000')
 
+    def create_config_dir(self, service_name):
+        os.system(
+            f'sudo mkdir -p {self.root_dir}docker/{service_name}-config'
+            ' && sudo chown -R {service_name}:mediacenter {self.root_dir}docker/{service_name}-config'
+        )
+
     def sonarr(self):
-        os.system('sudo useradd sonarr -u 13001 && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/tv && sudo mkdir -p ' + self.root_dir + 'docker/sonarr-config')
+        os.system(
+            'sudo useradd sonarr -u 13001'
+            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/tv -m 775'
+        )
+        self.create_config_dir('sonarr')
         os.system('sudo usermod -a -G mediacenter sonarr')
 
     def radarr(self):
-        os.system('sudo useradd radarr -u 13002 && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/movies && sudo mkdir -p ' + self.root_dir + 'docker/radarr-config')
+        os.system(
+            'sudo useradd radarr -u 13002'
+            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/movies -m 775'
+        )
+        self.create_config_dir('radarr')
         os.system('sudo usermod -a -G mediacenter radarr')
 
     def lidarr(self):
-        os.system('sudo useradd lidarr -u 13003 && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/music && sudo mkdir -p ' + self.root_dir + 'docker/lidarr-config')
+        os.system(
+            'sudo useradd lidarr -u 13003'
+            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/music -m 775'
+        )
+        self.create_config_dir('lidarr')
         os.system('sudo usermod -a -G mediacenter lidarr')
 
     def readarr(self):
-        os.system('sudo useradd readarr -u 13004 && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/books && sudo mkdir -p ' + self.root_dir + 'docker/readarr-config')
+        os.system(
+            'sudo useradd readarr -u 13004'
+            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/books -m 775'
+        )
+        self.create_config_dir('readarr')
         os.system('sudo usermod -a -G mediacenter readarr')
 
     def mylar3(self):
-        os.system('sudo useradd mylar -u 13005 && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/comics && sudo mkdir -p ' + self.root_dir + 'docker/mylar-config')
+        os.system(
+            'sudo useradd mylar -u 13005'
+            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/comics -m 775'
+        )
+        self.create_config_dir('mylar')
         os.system('sudo usermod -a -G mediacenter mylar')
 
     def prowlarr(self):
-        os.system('sudo useradd prowlarr -u 13006 && sudo mkdir -p ' + self.root_dir + 'docker/prowlarr-config')
+        os.system('sudo useradd prowlarr -u 13006')
+        self.create_config_dir('prowlarr')
         os.system('sudo usermod -a -G mediacenter prowlarr')
 
     def qbittorrent(self):
