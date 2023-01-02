723f4aba0fa2e89babccdd9e2332d32e1da3ff8f
Andrew Hannum <andrew.hannum@gmail.com>
2023-01-02 09:56:20 -0700

    Fix some problems with directory creation via Python setup script. (#18)
    * Fix broken create_config_dir() function.
    
    The second line of a multi-line string has replacement fields, but was not an f-string.
    
    * Top-level directories weren't created as intended.

--- users_groups_setup.py
+++ users_groups_setup.py
@@ -8,14 +8,14 @@ class UserGroupSetup:
 
     def create_config_dir(self, service_name):
         os.system(
-            f'sudo mkdir -p {self.root_dir}docker/{service_name}-config'
-            ' && sudo chown -R {service_name}:mediacenter {self.root_dir}docker/{service_name}-config'
+            f'sudo mkdir -p {self.root_dir}/docker/{service_name}-config'
+            f' && sudo chown -R {service_name}:mediacenter {self.root_dir}/docker/{service_name}-config'
         )
 
     def sonarr(self):
         os.system(
             'sudo useradd sonarr -u 13001'
-            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/tv -m 775'
+            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/tv -m 775'
         )
         self.create_config_dir('sonarr')
         os.system('sudo usermod -a -G mediacenter sonarr')
@@ -23,7 +23,7 @@ class UserGroupSetup:
     def radarr(self):
         os.system(
             'sudo useradd radarr -u 13002'
-            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/movies -m 775'
+            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/movies -m 775'
         )
         self.create_config_dir('radarr')
         os.system('sudo usermod -a -G mediacenter radarr')
@@ -31,7 +31,7 @@ class UserGroupSetup:
     def lidarr(self):
         os.system(
             'sudo useradd lidarr -u 13003'
-            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/music -m 775'
+            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/music -m 775'
         )
         self.create_config_dir('lidarr')
         os.system('sudo usermod -a -G mediacenter lidarr')
@@ -39,7 +39,7 @@ class UserGroupSetup:
     def readarr(self):
         os.system(
             'sudo useradd readarr -u 13004'
-            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/books -m 775'
+            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/books -m 775'
         )
         self.create_config_dir('readarr')
         os.system('sudo usermod -a -G mediacenter readarr')
@@ -47,7 +47,7 @@ class UserGroupSetup:
     def mylar3(self):
         os.system(
             'sudo useradd mylar -u 13005'
-            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/comics -m 775'
+            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/comics -m 775'
         )
         self.create_config_dir('mylar')
         os.system('sudo usermod -a -G mediacenter mylar')
@@ -55,7 +55,7 @@ class UserGroupSetup:
     def audiobookshelf(self):
         os.system(
             'sudo useradd audiobookshelf -u 13009'
-            ' && sudo mkdir -pv ' + self.root_dir + 'data/{media,torrents}/audiobooks -m 775'
+            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/audiobooks -m 775'
         )
         self.create_config_dir('audiobookshelf')
         os.system('sudo usermod -a -G mediacenter audiobookshelf')
