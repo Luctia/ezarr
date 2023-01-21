6ac67a4393f5d0d2052ff011a3b1314be49e3ba9
Luctia <timmerman.luc1999@gmail.com>
2023-01-21 01:30:55 +0100

    Fix some issues with shell commands in CLI version, fixing #19

--- users_groups_setup.py
+++ users_groups_setup.py
@@ -9,53 +9,53 @@ class UserGroupSetup:
     def create_config_dir(self, service_name):
         os.system(
             f'sudo mkdir -p {self.root_dir}/docker/{service_name}-config'
-            f' && sudo chown -R {service_name}:mediacenter {self.root_dir}/docker/{service_name}-config'
+            f' ; sudo chown -R {service_name}:mediacenter {self.root_dir}/docker/{service_name}-config'
         )
 
     def sonarr(self):
         os.system(
-            'sudo useradd sonarr -u 13001'
-            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/tv -m 775'
+            '/bin/bash -c "sudo useradd sonarr -u 13001'
+            ' ; sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/tv -m 775"'
         )
         self.create_config_dir('sonarr')
         os.system('sudo usermod -a -G mediacenter sonarr')
 
     def radarr(self):
         os.system(
-            'sudo useradd radarr -u 13002'
-            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/movies -m 775'
+            '/bin/bash -c "sudo useradd radarr -u 13002'
+            ' ; sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/movies -m 775"'
         )
         self.create_config_dir('radarr')
         os.system('sudo usermod -a -G mediacenter radarr')
 
     def lidarr(self):
         os.system(
-            'sudo useradd lidarr -u 13003'
-            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/music -m 775'
+            '/bin/bash -c "sudo useradd lidarr -u 13003'
+            ' ; sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/music -m 775"'
         )
         self.create_config_dir('lidarr')
         os.system('sudo usermod -a -G mediacenter lidarr')
 
     def readarr(self):
         os.system(
-            'sudo useradd readarr -u 13004'
-            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/books -m 775'
+            '/bin/bash -c "sudo useradd readarr -u 13004'
+            ' ; sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/books -m 775"'
         )
         self.create_config_dir('readarr')
         os.system('sudo usermod -a -G mediacenter readarr')
 
     def mylar3(self):
         os.system(
-            'sudo useradd mylar -u 13005'
-            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/comics -m 775'
+            '/bin/bash -c "sudo useradd mylar -u 13005'
+            ' ; sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/comics -m 775"'
         )
         self.create_config_dir('mylar')
         os.system('sudo usermod -a -G mediacenter mylar')
 
     def audiobookshelf(self):
         os.system(
-            'sudo useradd audiobookshelf -u 13009'
-            ' && sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/audiobooks -m 775'
+            '/bin/bash -c "sudo useradd audiobookshelf -u 13009'
+            ' ; sudo mkdir -pv ' + self.root_dir + '/data/{media,torrents}/audiobooks -m 775"'
         )
         self.create_config_dir('audiobookshelf')
         os.system('sudo usermod -a -G mediacenter audiobookshelf')
