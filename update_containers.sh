4d513a1d8f2f037750ff3f21c1c3210f52d72cb7
Pavlogal <pavledosen.p@gmail.com>
2024-05-03 23:15:44 +0200

    fix update_containers

--- update_containers.sh
+++ update_containers.sh
@@ -3,7 +3,13 @@
 # Shutds down all the containers, updates them and removes the old images. 
 # If you want to keep the old images remove or comment the last line.
 
-sudo docker-compose pull
-sudo docker-compose down
-sudo docker-compose up -d
-sudo docker image prune -f
+# If the following commands throw permission errors, uncomment the following lines
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
+#newgrp docker
+# if that isnt enough then reboot
+
+docker compose pull
+docker compose down
+docker compose up -d
+docker image prune -f
